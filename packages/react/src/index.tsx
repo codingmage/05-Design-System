f1e6eae77c1c18ba6c68adbad5b297a6cf6ce784
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-03-04 13:52:54 -0300

    add storybook and button

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,11 @@
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
-  borderRadius: '$md',
-  padding: '$4',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  padding: '$2 $4',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
 })
-
-export function App() {
-  return <Button>Hello world</Button>
-}
