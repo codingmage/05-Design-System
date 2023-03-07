591d79be48588ad9c9528b1e82dea8bef624735f
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-03-07 16:42:37 -0300

    add component Box and Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ipsum non tenetur aspernatur sapiente vel nobis culpa quaerat temporibus sed dicta quis modi nesciunt excepturi nulla, necessitatibus rerum assumenda deleniti.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
